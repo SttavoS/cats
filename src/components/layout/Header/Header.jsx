2196a32585c82e80a9101ac332a751fb08fb1cb2
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2024-08-21 23:20:45 -0300

    finish Header component

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -1,12 +1,17 @@
 import { Link } from 'react-router-dom';
 import styles from './Header.module.css';
+import CatsLogo from '../../../assets/icons/dogs.svg?react';
 
 const Header = () => {
   return (
-    <header>
-      <nav className="container">
-        <Link to="/">Home</Link>
-        <Link to="login">Criar/Login</Link>
+    <header className={styles.header}>
+      <nav className={`container ${styles.navigation}`}>
+        <Link to="/" className={styles.logo} aria-label="Cats | Home">
+          <CatsLogo />
+        </Link>
+        <Link to="login" className={styles.login}>
+          Criar/Login
+        </Link>
       </nav>
     </header>
   );
